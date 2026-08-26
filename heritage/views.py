c51001010ca0c171a82187f3f9f3a4fd0c29e0ab
Rajesh <dpsrajesh841@gmail.com>
2026-08-26 19:42:53 +0530

    Fix heritage record viewset

--- heritage/views.py
+++ heritage/views.py
@@ -1,26 +1,55 @@
 from rest_framework import viewsets, filters
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
-from .models import HeritageRecord, Category, Language, Location
-from .serializers import HeritageRecordSerializer, CategorySerializer, LanguageSerializer, LocationSerializer
+
+from .models import (
+    HeritageRecord,
+    Category,
+    Language,
+    Location,
+)
+
+from .serializers import (
+    HeritageRecordSerializer,
+    CategorySerializer,
+    LanguageSerializer,
+    LocationSerializer,
+)
+
 
 class CategoryViewSet(viewsets.ModelViewSet):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
 
+
 class LanguageViewSet(viewsets.ModelViewSet):
     queryset = Language.objects.all()
     serializer_class = LanguageSerializer
+
 
 class LocationViewSet(viewsets.ModelViewSet):
     queryset = Location.objects.all()
     serializer_class = LocationSerializer
 
+
 class HeritageRecordViewSet(viewsets.ModelViewSet):
-    queryset = HeritageRecord.objects.all().order_by('-created_at')
+    queryset = HeritageRecord.objects.all().order_by("-created_at")
     serializer_class = HeritageRecordSerializer
+
     permission_classes = [IsAuthenticatedOrReadOnly]
-    filter_backends = [filters.SearchFilter]
-    search_fields = ['title', 'description', 'category__name', 'language__name', 'location__district']
+
+    filter_backends = [
+        filters.SearchFilter,
+    ]
+
+    search_fields = [
+        "title",
+        "description",
+        "category__name",
+        "language__name",
+        "location__district",
+    ]
 
     def perform_create(self, serializer):
-        serializer.save(contributor=self.request.user)
+        serializer.save(
+            contributor=self.request.user
+        )
